ace3fdfcea4f650f01511b8a7a6f25d6cea00176
AndreaSdr70 <andrea@sdringola.it>
2024-08-08 21:50:56 +0200

    Selfwork PHP 1 - Corretto

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+// $integer = 15;
+// $float = 12.50;
+// $string = "Ciao a tutti";
+// $boolean = true;
+
+// var_dump ($integer);
+// var_dump ($float);
+// var_dump ($string);
+// var_dump ($boolean);
+
+const INTERO = 15;
+const VIRGOLA = 12.50;
+const STRINGA = "Ciao a tutti";
+const BOOLEANO = true;
+
+var_dump(INTERO);
+var_dump(VIRGOLA);
+var_dump(STRINGA);
+var_dump(BOOLEANO);
+
+?>
